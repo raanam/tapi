5ab31e84287082483bdc4b09b4ff8a990e2e71fe
Ram Anam <ramanamgeo@gmail.com>
2020-12-21 15:06:15 +1100

    User service test cases

---
+++ TrolleyApi.Tests/Common/FixtureBuilder.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+
+namespace TrolleyApi.Tests.Common
+{
+    public static class FixtureBuilder
+    {
+        public static IFixture Build()
+        {
+            var fixture = new Fixture().Customize(new AutoNSubstituteCustomization());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            return fixture;
+        }
+    }
+}
---
+++ TrolleyApi.Tests/User/UserServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrolleyApi.Tests.Common;
+using TrolleyApi.User;
+using AutoFixture;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace TrolleyApi.Tests.User
+{
+    public class UserServiceTests
+    {
+        private readonly IUserService _sut;
+
+        public UserServiceTests()
+        {
+            var fixtrue = FixtureBuilder.Build();
+            _sut = fixtrue.Create<UserService>();
+        }
+
+        [Fact]
+        public void When_Get_Returns_ValidResponse()
+        {
+            // Act.
+            var response = _sut.Get();
+
+            // Assert.
+            response.Name.Should().Be("Ram Anam");
+            response.Token.Should().Be("dab78cfc-6bd8-428f-be75-19be7bd38643");
+        }
+    }
+}
